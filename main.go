73fb1d20b8f7c1ca4b7bb4f527d908508e176e82
Edwin Dominguez <edwindominguez@hotmail.com>
2020-07-15 21:08:59 -0500

    log the error

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	file, err := os.Open(os.Args[1])
 	if err != nil {
-		fmt.Println("There was an error reading the file")
+		fmt.Println("There was an error reading the file:", err)
 		os.Exit(1)
 	}
 	io.Copy(os.Stdout, file)
